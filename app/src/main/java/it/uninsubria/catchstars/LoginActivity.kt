ab9cecd67e8482b36ffc08a0f42eba5e64fea7cc
GDev96 <frattini.gaia@libero.it>
2023-05-22 17:49:15 +0200

    firebase e login

--- app/src/main/java/it/uninsubria/catchstars/LoginActivity.kt
+++ app/src/main/java/it/uninsubria/catchstars/LoginActivity.kt
@@ -1,12 +1,67 @@
 package it.uninsubria.catchstars
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {        //creazione dell'activity con collegamento all'activity 'login'
+    private lateinit var insertEmail: EditText
+    private lateinit var insertPass: EditText
+    private lateinit var LogInButton: Button
+    private lateinit var SignInButton: Button
+
+    private lateinit var auth: FirebaseAuth;
+
+    //creazione dell'activity con collegamento al layout 'login'
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
+
+        //collegamento a firebase
+        auth = Firebase.auth
+
+        //associazione oggetti layout
+        insertEmail = findViewById(R.id.username)
+        insertPass = findViewById(R.id.password)
+        LogInButton = findViewById(R.id.login)
+        SignInButton = findViewById(R.id.register)
+
+        LogInButton.setOnClickListener{
+            //to do: controllo credenziali e accesso
+            val email = insertEmail.text.toString()
+            val password = insertPass.text.toString()
+
+
+
+            //Toast.maketext permette di creare un popup con testo
+
+
+            //chiamata metodo per il passaggio a HomeGame
+            goHomeGame()
+        }
+
+        SignInButton.setOnClickListener{
+            //passaggio alla schermata SignIn
+            goSignUp()
+        }
+
     }
+
+    //metodo per il passaggio alla schermata home del gioco
+    fun goHomeGame(){
+        val intent = Intent(this@LoginActivity, HomeGameActivity::class.java)
+        HomeGameActivity(intent)
+    }
+
+    fun goSignUp(){
+        val intent = Intent(this@LoginActivity, SignUpActivity::class.java)
+        SignUpActivity(intent)
+    }
+
 }
--- app/src/main/java/it/uninsubria/catchstars/MainActivity.kt
+++ app/src/main/java/it/uninsubria/catchstars/MainActivity.kt
@@ -2,26 +2,29 @@ package it.uninsubria.catchstars
 
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    lateinit var startImageButton: ImageButton //dichiarazione di una variabile
+
+    private lateinit var startImageButton: ImageButton //dichiarazione di una variabile
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        startImageButton = findViewById(R.id.Start_button) //assegnazione alla variabile dell'oggetto (pulsante)
+        //assegnazione alla variabile dell'oggetto (button)
+        startImageButton = findViewById(R.id.Start_button)
 
+        //assegnazione(chiamata) del metodo al button
+        startImageButton.setOnClickListener{
+            goLoginPage()
+        }
     }
 
-    fun goLoginPage(v: View) {  //metodo per il passaggio alla schermata di Login
+    //metodo per il passaggio alla schermata di Login
+    fun goLoginPage() {
         val intent = Intent(this@MainActivity, LoginActivity::class.java)
         startActivity(intent)
     }
-
-    //assegnazione del metodo al pulsante?
-
 }
