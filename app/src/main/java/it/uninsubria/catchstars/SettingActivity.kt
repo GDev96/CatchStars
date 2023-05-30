2af0ed929f8ee61c8e0c61594e5075f37d9d61ff
GDev96 <frattini.gaia@libero.it>
2023-05-30 17:22:00 +0200

    Update kt e xml
    add info button setting xml, change button homegame xml. add comment setting kt

--- app/src/main/java/it/uninsubria/catchstars/SettingActivity.kt
+++ app/src/main/java/it/uninsubria/catchstars/SettingActivity.kt
@@ -3,3 +3,9 @@ package it.uninsubria.catchstars
 import android.app.Activity
 
 class SettingActivity : Activity()
+
+
+
+/*pulsante info
+apertura pop up: nome progetto,     nome sviluppatore e matricola, data di rilascio, versione
+ */
