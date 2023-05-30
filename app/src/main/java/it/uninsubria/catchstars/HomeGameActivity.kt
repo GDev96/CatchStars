c78a13c54469b9f5e486bfa73dfb37c0e0930548
GDev96 <frattini.gaia@libero.it>
2023-05-30 16:41:14 +0200

    Update HomeGameActivity.kt
    - intent pulsanti
    - logout firebase

--- app/src/main/java/it/uninsubria/catchstars/HomeGameActivity.kt
+++ app/src/main/java/it/uninsubria/catchstars/HomeGameActivity.kt
@@ -1,5 +1,66 @@
 package it.uninsubria.catchstars
 
-import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
-class HomeGameActivity : Activity()
+class HomeGameActivity(intent: Intent) : AppCompatActivity() {
+
+    private lateinit var PlayButton: Button
+    private lateinit var SettingButton: Button
+    private lateinit var ScoreButton: Button
+    private lateinit var LogOutButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.homegame_activity)
+
+        //associazione button/variabili
+        PlayButton = findViewById(R.id.playgame)
+        SettingButton = findViewById(R.id.setting)
+        ScoreButton = findViewById(R.id.score)
+        LogOutButton = findViewById(R.id.exit)
+
+        //button play
+        PlayButton.setOnClickListener {
+            val intent = Intent(this@HomeGameActivity, GameActivity::class.java)
+            startActivity(intent)
+        }
+
+        //button setting
+        SettingButton.setOnClickListener{
+            val intent = Intent (this@HomeGameActivity, SettingActivity::class.java)
+            startActivity(intent)
+        }
+
+        //button score
+        ScoreButton.setOnClickListener{
+            val intent = Intent (this@HomeGameActivity, ScoreActivity::class.java)
+            startActivity(intent)
+        }
+
+        //exitbutton -> logout firebase e ritorno a schermata di avvio
+        LogOutButton.setOnClickListener{
+            //logout firebase
+            Firebase.auth.signOut()
+            //creazione pop up(?) "sei sicuro di voler uscire?" due pulsanti: exit -> mainactivity, back -> homegameactivity
+            val intent = Intent(this@HomeGameActivity, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
